40c57233f732232184c0c622e6d14a016576fbd4
Kucherjashka <vnoir20@gmail.com>
2022-04-08 01:40:11 +0300

    Add user model + validation, add moodels dependenses, db update

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  EMAIL_VALIDATION_REGEX = URI::MailTo::EMAIL_REGEXP
+
+  has_many :events
+
+  validates :name, presence: true, length: { maximum: 35 }
+  validates :email, presence: true, length: {maximum: 255}
+  validates :email, uniqueness: true
+  validates :email, format: { with: EMAIL_VALIDATION_REGEX }
+end
