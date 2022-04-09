fa111cffaf41a5313d0c88749eae42c9ffcf31de
Kucherjashka <vnoir20@gmail.com>
2022-04-09 14:24:20 +0300

    Turn on action mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  EMAIL_VALIDATION_REGEX = URI::MailTo::EMAIL_REGEXP
-
   has_many :events
 
   validates :name, presence: true, length: { maximum: 35 }
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: { with: EMAIL_VALIDATION_REGEX }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = email.split('@').first if name.blank?
+  end
 end
