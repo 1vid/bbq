62290e51a3a980520a3a63f5e143548f837bd1fe
Kucherjashka <vnoir20@gmail.com>
2022-04-11 10:26:10 +0300

    Add anon subscriptions will links to a new user with the same email

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,15 @@ class User < ApplicationRecord
 
   before_validation :set_name, on: :create
 
+  after_comit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = email.split('@').first if name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
   end
 end
