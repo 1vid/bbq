e501ddfc824311b2095c7a3051a221645f776065
Kucherjashka <vnoir20@gmail.com>
2022-04-16 15:26:13 +0300

    destroy & update bug fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    if @user.destroy
+        redirect_to root_url, notice: "User deleted."
+    end
+  end
+
   private
 
   def set_current_user
